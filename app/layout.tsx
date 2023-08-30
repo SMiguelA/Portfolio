00274ece3e40af225c517fb57c4295ba58b576e8
Miguel Sierra <sierrabolanosmiguel@gmail.com>
2023-08-30 13:59:03 -0500

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <title>Miguel Sierra Portfolio</title>
         <link rel="shortcut icon" href="/favicon.svg" />
+        <title>Miguel Sierra</title>
+        <meta property="og:title" content="Miguel Sierra" key="title"/>
+      </Head>
+      <Head>
+        <meta property="og:title" content="Miguel Sierra" key="title" />
+      </Head>
+      <Head>
         <meta name="description" content="Pagina de presentación Miguel Angel Sierra" />
-        <meta property="og:title" content="Miguel Sierra Portfolio" />
         <meta property="og:description" content="Pagina de presentación Miguel Angel Sierra" />
       </Head>
       <Providers>
